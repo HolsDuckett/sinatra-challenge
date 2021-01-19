5ba7baa3edcd27694237783c0e45d2e60ab12ef3
Miranda Wilson <mirandascwilson@gmail.com>
2021-01-19 15:31:37 +0000

    created views folder and index.erb

--- app.rb
+++ app.rb
@@ -10,5 +10,5 @@ get '/secret' do
 end
 
 get '/cat' do
-  "<div style = 'border: 5px dashed pink'> <img src='https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png' alt='cute cat'> </div>"
+  erb(:index)
 end
