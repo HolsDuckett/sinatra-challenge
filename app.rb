20e3c1e705e8b9f9a23cb1c2f2a820021c4585fb
Holsduckett <hf.duckett@gmail.com>
2021-01-19 16:41:31 +0000

    created a new route /named-cat and used parameters

--- app.rb
+++ app.rb
@@ -9,7 +9,14 @@ get '/secret' do
   "You know all my secrets"
 end
 
-get '/cat' do
+get '/random-cat' do
   @name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
+  @age = params[:age]
   erb(:index)
 end
