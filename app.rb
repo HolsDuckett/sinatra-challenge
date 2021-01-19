bc3a8dd92cdc3a25b3de57c00d816912a2c05cf4
Holsduckett <hf.duckett@gmail.com>
2021-01-19 14:58:52 +0000

    added a cat image with a pink border using CSS and HTML

--- app.rb
+++ app.rb
@@ -8,3 +8,7 @@ end
 get '/secret' do
   "You know all my secrets"
 end
+
+get '/cat' do
+  "<div style = 'border: 5px dashed pink'> <img src='https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png' alt='cute cat'> </div>"
+end
